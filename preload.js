a14f1b55ebce1d28ab761b222bf77df1f0bfcb9d
ryuyutyo <liuyouchao111@gmail.com>
2024-03-02 23:22:25 +0800

    feat: using preload script

---
+++ preload.js
@@ -0,0 +1,19 @@
+// window.addEventListener('DOMContentLoaded', () => {
+//   const replaceText = (selector, text) => {
+//     const element = document.getElementById(selector)
+//     if (element) element.innerText = text
+//   }
+
+//   for (const type of ['chrome', 'node', 'electron']) {
+//     replaceText(`${type}-version`, process.versions[type])
+//   }
+// })
+
+
+const { contextBridge } = require('electron/renderer')
+
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron
+})
